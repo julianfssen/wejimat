aa2f52f906e6434649a8452b7aa802c4849d8733
julianfoo <julian.foo@investingnote.com>
2020-12-08 21:09:51 +0800

    Added API to fetch list of transactions

--- client/src/components/Transaction.js
+++ client/src/components/Transaction.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Transaction() {
+	const [textInput, setTextInput] = useState('');
+	const [transactions, setTransactions] = useState([]);
+
+	const callApi = (req) => {
+		fetch(req)
+			.then(response => response.json())
+			.then(txns => txns.map(txn => setTransactions([...transactions, {txn}])))
+	}
+
 	return (
 		<div>
-			<h1>Hello.</h1>
+			<h1>What did you spend on?</h1>
+			<button>Add</button>
+			<button
+				onClick={() => callApi('http://localhost:3000/api/v1/transactions')}
+			>
+			  My transactions
+			</button>
+			<div>
+				<ul>
+					{transactions.map(txn => <li key={txn.txn.id}>{txn.txn.name}</li>)}
+				</ul>
+			</div>
 		</div>
 	)
 }
