99da484fa011f4bd75d780d679f66b05ca94a739
julianfoo <julian.foo@investingnote.com>
2020-12-11 10:14:56 +0800

    Added submit button to add transactions

--- client/src/components/Transaction.js
+++ client/src/components/Transaction.js
@@ -9,16 +9,35 @@ function Transaction() {
 			.then(response => response.json())
 			.then(txns => setTransactions(txns))
 	}
+	
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(textInput);
+	}
 
 	return (
 		<div>
 			<h1>What did you spend on?</h1>
-			<button>Add</button>
-			<button
-				onClick={() => callApi('http://localhost:3000/api/v1/transactions')}
-			>
-			  My transactions
-			</button>
+			<form onSubmit={handleSubmit}>
+			  <input
+			  	type='text'
+			  	name='transaction'
+			    placeholder='I spent on...'
+			  	value={textInput}
+			  	onChange={e => setTextInput(e.target.value)}
+			  >
+			  </input>
+			  <button
+					type='submit'
+			  >
+			    Add
+			  </button>
+			  <button
+			  	onClick={() => callApi('http://localhost:3000/api/v1/transactions')}
+			  >
+			    My transactions
+			  </button>
+			</form>
 			<div>
 				<ul>
 					{transactions.map(txn => <li key={txn.id}>{txn.name}</li>)}
