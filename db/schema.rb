9d2c079f6b272bf32a5e930ffba554e95f6c2382
julianfssen <julianfssen@gmail.com>
2020-12-25 17:58:12 +0800

    Changed DB to use Postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_12_23_012405) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "expenses", force: :cascade do |t|
     t.string "name"
     t.integer "amount"
