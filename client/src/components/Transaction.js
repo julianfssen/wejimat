6bac138946bc1a2789bacce1b4fc88e122fe9f95
julianfssen <julianfssen@gmail.com>
2020-12-29 21:44:38 +0800

    WIP: Styling transactions page

--- client/src/components/Transaction.js
+++ client/src/components/Transaction.js
@@ -138,72 +138,94 @@ function Transaction() {
 
 	return (
 		<div className='mx-auto w-1/2'>
-			<h1 className='my-5 text-purple-700 font-bold text-center text-2xl'>What did you spend on?</h1>
-			<form onSubmit={e => handleSubmit(e)}>
-			  <input
-			  	type='text'
-			  	name='transactionName'
-			    placeholder='I spent on...'
-			  	value={transactionName}
-			  	onChange={e => setTransactionName(e.target.value)}
-				className='mx-4 border rounded-md focus:ring-2 focus:ring-purple-600 shadow-md placeholder-gray-600'
-			  >
-			  </input>
-			  <input
-			  	type='text'
-			  	name='transactionAmount'
-			    placeholder='For RM...'
-					value={transactionAmount > 0 ? transactionAmount : ''}
-			  	onChange={e => setTransactionAmount(e.target.value)}
-				className='mx-4 w-24 border rounded-md focus:ring-2 focus:ring-purple-600 shadow-md placeholder-gray-600'
-			  >
-			  </input>
-				<select
-				  name='paymentChannels'
-				  onChange={e => setSelectedPaymentChannel(e.target.value)}
-				  className='inline-flex justify-center w-1/8 rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500 cursor-pointer'
-				>
-				  {paymentChannels.map((channel, index) => <option value={channel} key={channel}>{channel}</option>)}
-				</select>
-			  <input
-				  className='mx-4 border rounded-md bg-purple-600 text-white font-medium p-1.5 px-5 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 cursor-pointer'
-				  type='submit'
-				  value='Add Expense'
-			  />
-			</form>
-			<button
-			  onClick={() => viewAllTransactions()}
-			>
-			  All Transactions
-			</button>
-			<h3>
-				View expenses by channel
-			</h3>
-			<select
-				name='transactionsByPaymentChannel'
-				onChange={e => handleChannelChange(e.target.value)}
-			>
-				{paymentChannels.map(channel => <option value={channel} key={channel}>{channel}</option>)}
-			</select>
-			<h3>
-				View expenses by month
-			</h3>
-			<select
-				name='transactionsByMonth'
-				onChange={e => handleMonthChange(e.target.value)}
-			>
-				{MONTHS.map(month=> <option value={month} key={month}>{month}</option>)}
-			</select>
-			<input 
-			  label='date'
-			  type='date'
-			  onChange={e => handleDateChange(e.target.value)}>
-			</input>
-			<div>
-				<ul>
-					{transactions.map(txn => <li key={txn.id}>{txn.name}: {txn.amount}</li>)}
-				</ul>
+		  <div className='add-expense-form mx-auto w-3/4'>
+		    <h1 className='my-5 text-purple-700 font-bold text-center text-2xl'>
+			  What did you spend on today?
+			</h1>
+		    <form onSubmit={e => handleSubmit(e)}>
+		      <input
+		      	type='text'
+		      	name='transactionName'
+		        placeholder='I spent on...'
+		      	value={transactionName}
+		      	onChange={e => setTransactionName(e.target.value)}
+		    	className='mx-2 px-2 border rounded-md focus:ring-2 focus:ring-purple-600 shadow-md placeholder-gray-600'
+		      >
+		      </input>
+		      <input
+		      	type='text'
+		      	name='transactionAmount'
+		        placeholder='For RM...'
+		    		value={transactionAmount > 0 ? transactionAmount : ''}
+		      	onChange={e => setTransactionAmount(e.target.value)}
+		    	className='mx-2 px-2 w-24 border rounded-md focus:ring-2 focus:ring-purple-600 shadow-md placeholder-gray-600'
+		      >
+		      </input>
+		    	<select
+		    	  name='paymentChannels'
+		    	  onChange={e => setSelectedPaymentChannel(e.target.value)}
+		    	  className='inline-flex justify-center w-1/8 rounded-md border border-gray-300 shadow-sm mx-2 px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500 cursor-pointer'
+		    	>
+		    	  {paymentChannels.map((channel, index) => <option value={channel} key={channel}>{channel}</option>)}
+		    	</select>
+		      <input
+		    	  className='mx-2 border rounded-md bg-purple-600 text-white font-medium p-1.5 px-5 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 cursor-pointer'
+		    	  type='submit'
+		    	  value='Add Expense'
+		      />
+		    </form>
+		  </div>
+		  <div className='expenses-management container mx-auto'>
+			<h1 className='mx-5 my-5 text-purple-700 font-bold text-center text-2xl'>
+			  My Expenses
+			</h1>
+		    <button
+		      className='view-all-expenses-btn mx-auto border rounded-md bg-purple-600 text-white font-medium p-1.5 px-5 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 cursor-pointer'
+		      onClick={() => viewAllTransactions()}
+		    >
+		      All Expenses
+		    </button>
+		    <h3 className='mx-5 my-5 text-purple-700 font-semibold text-center text-xl'>
+		    	View expenses by payment providers
+		    </h3>
+		    <select
+		    	name='transactionsByPaymentChannel'
+		    	onChange={e => handleChannelChange(e.target.value)}
+		    	className='view-expenses-by-channel-btn inline-flex justify-center w-1/8 rounded-md border border-gray-300 shadow-sm mx-2 px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500 cursor-pointer'
+		    >
+		    	{paymentChannels.map(channel => <option value={channel} key={channel}>{channel}</option>)}
+		    </select>
+			<div className='view-expenses-by-month mx-auto'>
+		      <h3 className='mx-5 my-5 text-purple-700 font-semibold text-center text-xl'>
+		      	View expenses by month
+		      </h3>
+		      <select
+		      	name='transactionsByMonth'
+		      	onChange={e => handleMonthChange(e.target.value)}
+		    	className='view-expenses-by-month-btn inline-flex justify-center w-1/8 rounded-md border border-gray-300 shadow-sm mx-2 px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500 cursor-pointer'
+		      >
+		      	{MONTHS.map(month=> <option value={month} key={month}>{month}</option>)}
+		      </select>
 			</div>
+			<div className='view-expenses-by-date mx-auto'>
+		      <h3 className='mx-5 my-5 text-purple-700 font-semibold text-center text-xl'>
+		      	View expenses by date
+		      </h3>
+		      <input 
+		        label='date'
+		        type='date'
+		        onChange={e => handleDateChange(e.target.value)}
+				defaultValue={new Date().toISOString().slice(0, 10)}
+		    	className='view-expenses-by-date-btn inline-flex justify-center w-1/8 rounded-md border border-gray-300 shadow-sm mx-2 px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500 cursor-pointer'
+			  >
+		      </input>
+			</div>
+		  </div>
+		  <div className='expenses-list'>
+		  	<ul>
+		  		{transactions.map(txn => <li key={txn.id}>{txn.name}: {txn.amount}</li>)}
+		  	</ul>
+		  </div>
 		</div>
 	)
 }
